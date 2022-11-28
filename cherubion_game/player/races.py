8e07714e03959f1613b4652e61a7ec59107e4c4b
happypingu <lajosleonardo01@gmail.com>
2022-11-28 08:46:04 +0100

    fix

--- cherubion_game/player/races.py
+++ cherubion_game/player/races.py
@@ -1,175 +1,141 @@
-'''
 from . import interface
-from .player.races import Player
 
-_ENTITY_DISPLAY_NAME = "You"
-
-_EEMSI = "eemsi"
-_KEOR = "keor"
-_HREEIR = "hreeir"
-_AZEN = "azen"
-_HHRGAK = "hhrgak"
-_RACES = [_EEMSI, _KEOR, _HREEIR, _AZEN, _HHRGAK]
-
-# DEFAULT HP: 20
-# DEFAULT STRENGTH: 14
-# DEFAULT DEFENCE: 14
-# DEFAULT LUCK: 9 || REMOVE 1 LUCK AFTER EVERY USE
-# ABILITY POWER: 20 || RESTORE DEFAULT AP AFTER REST
-
-_EEMSI_ENTITY = CombatEntity(
-    display_name=_ENTITY_DISPLAY_NAME,
-    max_hp=26,
-    strength=17,
-    defence=14,
-    luck=7,
-    magic=20,
-)
-
-_KEOR_ENTITY = CombatEntity(
-    display_name=_ENTITY_DISPLAY_NAME,
-    max_hp=28,
-    strength=11,
-    defence=16,
-    luck=9,
-    magic=20,
-)
-
-_HREEIR_ENTITY = CombatEntity(
-    display_name=_ENTITY_DISPLAY_NAME,
-    max_hp=24,
-    strength=14,
-    defence=14,
-    luck=8,
-    magic=20,
-)
-
-_AZEN_ENTITY = CombatEntity(
-    display_name=_ENTITY_DISPLAY_NAME,
-    max_hp=16,
-    strength=14,
-    defence=14,
-    luck=11,
-    magic=25,
-)
-
-_HHRGAK_ENTITY = CombatEntity(
-    display_name=_ENTITY_DISPLAY_NAME,
-    max_hp=18,
-    strength=11,
-    defence=15,
-    luck=11,
-    magic=28,
-)
-
-def create_player():
-    interface.print_multiple_lines([
-        "On your adventure you may be an Eemsi, Keor, Hreeir, Azen, or Hhrgak.",
-        "Which do you choose?"
-    ])
-
-    command = interface.get_command(_RACES)
-
-    interface.print_("You have chosen the race {}.".format(command))
-    interface.print_()
-
-    if command == _EEMSI:
-        return Player(_EEMSI_ENTITY, "eemsi")
-
-    if command == _KEOR:
-        return Player(_KEOR_ENTITY, "keor")
-
-    if command == _HREEIR:
-        return Player(_HREEIR_ENTITY, "hreeir")
-
-    if command == _AZEN:
-        return Player(_AZEN_ENTITY, "azen")
-
-    if command == _HHRGAK:
-        return Player(_HHRGAK_ENTITY, "hhrgak")
-'''
 RACES = ["eemsi", "keor", "hreeir", "azen", "hhrgak"]
 
+# TODO: add relevant data
 def create_race():
-    print("There are five races you can choose from: Eemsi, Keor, Hreeir, Azen, Hhrgak\n")
-    print("if u wanna see the info, say info and the name of the race")
-    print("if u wanna choose say choose and the name of the race")
+    interface.print_("There are five races you can choose from: Eemsi, Keor, Hreeir, Azen, Hhrgak\n")
+    interface.print_("if u wanna see the info, say info and the name of the race")
+    interface.print_("if u wanna choose say choose and the name of the race")
+
     while True:
         choose_or_info = input()
+        info_choose_commands = ['info eemsi', 'info keor', 'info hreeir', 'info azen', 'info hhrgak', 'choose eemsi', 'choose keor', 'choose hreeir', 'choose azen', 'choose hhrgak']
+        if choose_or_info.lower() == info_choose_commands[0]:
+            interface.print_()
+            interface.print_('Race: Eemsi')
+            interface.print_('Health:', 26)
+            interface.print_('Strength:', 17)
+            interface.print_('Defence:', 14)
+            interface.print_('Luck:', 7)
+            interface.print_('Magic:', 20)
+            interface.print_()
 
-        if choose_or_info.lower() == "info eemsi":
-            print(f'Health: {Eemsi.max_health}')
+        if choose_or_info.lower() == info_choose_commands[1]:
+            interface.print_()
+            interface.print_('Race: Keor')
+            interface.print_('Health:', 28)
+            interface.print_('Strength:', 11)
+            interface.print_('Defence:', 16)
+            interface.print_('Luck:', 9)
+            interface.print_('Magic:', 20)
+            interface.print_()
+
+        if choose_or_info.lower() == info_choose_commands[2]:
+            interface.print_()
+            interface.print_('Race: Hreeir')
+            interface.print_('Health:', 24)
+            interface.print_('Strength:', 14)
+            interface.print_('Defence:', 14)
+            interface.print_('Luck:', 8)
+            interface.print_('Magic:', 20)
+            interface.print_()
+
+        if choose_or_info.lower() == info_choose_commands[3]:
+            interface.print_()
+            interface.print_('Race: Azen')
+            interface.print_('Health:', 16)
+            interface.print_('Strength:', 14)
+            interface.print_('Defence:', 14)
+            interface.print_('Luck:', 11)
+            interface.print_('Magic:', 25)
+            interface.print_()
+
+        if choose_or_info.lower() == info_choose_commands[4]:
+            interface.print_()
+            interface.print_('Race: Hhrgak')
+            interface.print_('Health:', 18)
+            interface.print_('Strength:', 11)
+            interface.print_('Defence:', 15)
+            interface.print_('Luck:', 11)
+            interface.print_('Magic:', 28)
+            interface.print_()
 
 
-        race_prompt = input()
-        if race_prompt.lower() == "eemsi":
-            race_chosen = Eemsi.create_player
+class Hero():
+    def __init__(self, herorace, herohealth, herostrength, herodefence, heroluck, heromagic):
+        self.ourrace = herorace
 
-        if race_prompt.lower() == "keor":
-            race_chosen = Keor.create_player
+        self.basehealth = herohealth
+        self.health = self.basehealth
 
-        if race_prompt.lower() == "hreeir":
-            race_chosen = Hreeir.create_player
+        self.basestrength = herostrength
+        self.strength = self.basestrength
 
-        if race_prompt.lower() == "azen":
-            race_chosen = Azen.create_player
+        self.basedefence = herodefence
+        self.defence = self.basedefence
 
-        if race_prompt.lower() == "hhrgak":
-            race_chosen = Hhrgak.create_player
+        self.baseluck = heroluck
+        self.luck = self.baseluck
+
+        self.basemagic = heromagic
+        self.magic = self.basemagic
+
+
+# TODO: add relevant data
+def datadict(self):
+    return {
+        'Race': str(self.ourrace),
+        'Health': str(self.health),
+        'Strength': str(self.strength),
+        'Defence': str(self.defence),
+        'Luck': str(self.luck),
+        'Magic': str(self.magic)
+    }
+
+
+def raceperks(self):
+    if self.ourclass == 'eemsi':
+        self.basehealth = 26
+        self.basestrength = 17
+        self.basedefence = 14
+        self.baseluck = 7
+        self.basemagic = 20
     
-    return race_chosen
+    elif self.ourclass == 'keor':
+        self.basehealth = 28
+        self.basestrength = 11
+        self.basedefence = 16
+        self.baseluck = 9
+        self.basemagic = 20
 
-class Eemsi():
-    def create_player(self):
-        self.max_health = 26
-        self.health = self.max_health
-        self.strength = 17
-        self.defence = 14
-        self.luck = 7
-        self.magic = 20
-    
-    def show_info():
-        print(f'Health: ')
-        print(f'Strength: ')
-        print(f'Defence: ')
-        print(f'Luck: ')
-        print(f'Magic: ')
+    elif self.ourclass == 'hreeir':
+        self.basehealth = 24
+        self.basestrength = 14
+        self.basedefence = 14
+        self.baseluck = 8
+        self.basemagic = 20
 
-class Keor():
-    def create_player(self):
-        self.max_health = 28
-        self.health = self.max_health
-        self.strength = 11
-        self.defence = 16
-        self.luck = 9
-        self.magic = 20
+    elif self.ourclass == 'azen':
+        self.basehealth = 16
+        self.basestrength = 14
+        self.basedefence = 14
+        self.baseluck = 11
+        self.basemagic = 25
 
-
-class Hreeir():
-    def create_player(self):
-        self.max_health = 24
-        self.health = self.max_health
-        self.strength = 14
-        self.defence = 14
-        self.luck = 8
-        self.magic = 20
+    elif self.ourclass == 'hhrgak':
+        self.basehealth = 18
+        self.basestrength = 11
+        self.basedefence = 15
+        self.baseluck = 11
+        self.basemagic = 28
 
 
-class Azen():
-    def create_player(self):
-        self.max_health = 16
-        self.health = self.max_health
-        self.strength = 14
-        self.defence = 14
-        self.luck = 11
-        self.magic = 25
-
-
-class Hhrgak():
-    def create_player(self):
-        self.max_health = 18
-        self.health = self.max_health
-        self.strength = 11
-        self.defence = 15
-        self.luck = 11
-        self.magic = 28
+# TODO: add relevant data
+def print_info(self):
+    interface.print_('Race', str(self.ourrace))
+    interface.print_('Health', str(self.health))
+    interface.print_('Strength', str(self.strength))
+    interface.print_('Defence', str(self.defence))
+    interface.print_('Luck', str(self.luck))
+    interface.print_('Magic', str(self.magic))
