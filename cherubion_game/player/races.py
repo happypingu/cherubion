b401bc3d6bc43511cba65ce0c686dd3313eb66b0
happypingu <lajosleonardo01@gmail.com>
2022-12-07 17:35:43 +0100

    finished the hero class, fixed the stats command

--- cherubion_game/player/races.py
+++ cherubion_game/player/races.py
@@ -2,11 +2,7 @@ from cherubion_game import interface
 
 RACES = ["eemsi", "keor", "hreeir", "azen", "hhrgak"]
 
-
-# TODO: add relevant data
-
-def create_race():
-    race_info = {
+race_info = {
         "eemsi": {
             "health": 26,
             "strength": 17,
@@ -43,7 +39,10 @@ def create_race():
             "magic": 28
         },
     }
-    
+
+def create_race():
+    commands = ["info eemsi", "choose eemsi", "info keor", "choose keor", "info hreeir", "choose hreeir", "info azen", "choose azen", "info hhrgak", "choose hhrgak"]
+
     interface.print_("There are five races you can choose from: Eemsi, Keor, Hreeir, Azen, Hhrgak\n")
     interface.print_("To see information about a race, enter 'info' followed by the name of the race.")
     interface.print_("To choose a race, enter 'choose' followed by the name of the race.\n")
@@ -63,81 +62,39 @@ def create_race():
                 interface.print_()
 
             elif f"choose {race}" in user_input.lower():
-                return race
+                player = Hero(race)
+                return player
             
-        print('Not a valid command!')
+
+        if user_input not in commands:
+            print('Not a valid command!')
 
 
 class Hero:
     def __init__(self, herorace):
         self.ourrace = herorace
 
-        self.basehealth = 0
+        self.basehealth = race_info[self.ourrace]["health"]
         self.health = self.basehealth
 
-        self.basestrength = 0
+        self.basestrength = race_info[self.ourrace]["strength"]
         self.strength = self.basestrength
 
-        self.basedefence = 0
+        self.basedefence = race_info[self.ourrace]["defence"]
         self.defence = self.basedefence
 
-        self.baseluck = 0
+        self.baseluck = race_info[self.ourrace]["luck"]
         self.luck = self.baseluck
 
-        self.basemagic = 0
+        self.basemagic = race_info[self.ourrace]["magic"]
         self.magic = self.basemagic
     
     def __str__(self):
         return (
-            f"Race: {self.ourrace}\n"
+            f"Race: {self.ourrace.capitalize()}\n"
             f"Health: {self.health}\n"
             f"Strength: {self.strength}\n"
             f"Defence: {self.defence}\n"
             f"Luck: {self.luck}\n"
             f"Magic: {self.magic}\n"
         )
-
-    def raceperks(self):
-        race_info = {
-            "eemsi": {
-                "health": 26,
-                "strength": 17,
-                "defence": 14,
-                "luck": 7,
-                "magic": 20
-            },
-            "keor": {
-                "health": 28,
-                "strength": 11,
-                "defence": 16,
-                "luck": 9,
-                "magic": 20
-            },
-            "hreeir": {
-                "health": 24,
-                "strength": 14,
-                "defence": 14,
-                "luck": 8,
-                "magic": 20
-            },
-            "azen": {
-                "health": 16,
-                "strength": 14,
-                "defence": 14,
-                "luck": 11,
-                "magic": 28
-            },
-            "hhrgak": {
-                "health": 18,
-                "strength": 11,
-                "defence": 15,
-                "luck": 11,
-                "magic": 28
-            },
-        }
-
-        self.basehealth = race_info[self.ourrace]["health"]
-        self.basestrength = race_info[self.ourrace]["strength"]
-        self.basedefence = race_info[self.ourrace]["defence"]
-        self.baseluck = race_info[self.ourrace]["luck"]
-        self.basemagic = race_info[self.ourrace]["magic"]
